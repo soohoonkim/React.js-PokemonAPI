a59cdf3e8ae58c3d813594471474849e8dfce76d
soohoonkim <soohoon.kim@hotmail.com>
2021-10-08 22:13:37 +0900

    Created basic structure for random Pokemon generator with contructor with states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Item from './MyItem';
+import React from 'react';
+// import Item from './MyItem';
 
+class Pokemon extends React.Component {
+  
+  constructor() {
+    super()
+    this.state = {
+      name: null,
+      id: null,
+      type: [],
+      sprite: '',
+    }
+  }
+
+  getNewPokemon() {
+    console.log("getting a new pokemon...")
+    this.setState({
+      name: "Bulbasaur",
+      id: "1",
+      type: ["grass", "poison"],
+      sprite: "coming soon...",
+    })
+  }
+  
+  render () {
+    return (
+      <div>
+        <h1>Pokemon API</h1>
+        <p>{this.state.name}</p>
+        <p>Pokedex no.: {this.state.id}</p>
+        <ol> Type:
+          <li>{this.state.type}</li>
+          <li>{this.state.type}</li>
+        </ol>
+        <p>{this.state.sprite}</p>
+        <button
+          type="button"
+          onClick={() => this.getNewPokemon()}
+          className="btn"
+        >
+          Generate a random Pokemon
+        </button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Item name="Misty" />
-          <Item name="Aloe" />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Pokemon />
       </header>
     </div>
   );
